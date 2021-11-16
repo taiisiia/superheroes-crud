bfab27bab45872609bbed3a7b8c324d84ed34138
Taiisiia <melnikovataya@gmail.com>
2021-11-16 20:17:37 +0200

    npm init and config

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const app = require("./app");
+
+const PORT = 5000;
+
+http.createServer(app).listen(PORT, () => {
+    console.log(`App is listening on PORT ${PORT}`);
+});
